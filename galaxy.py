e003f0b09aebbb9c964d98a8c2230cebcaf1f5a2
vishal7732 <vishal773289@gmail.com>
2021-04-27 00:20:14 +0530

    First Version

---
+++ galaxy.py
@@ -0,0 +1,122 @@
+import pygame,os, random, math
+pygame.font.init()
+
+pygame.init()
+
+#game window
+width = 900
+height = 750
+gameWindow = pygame.display.set_mode((width,height))        # for game window
+pygame.display.set_caption("Corona Wars") 					#for game window title
+logo = pygame.image.load("images/virus.png")
+pygame.display.set_icon(logo) 								#for game window title
+
+exit_game = False
+game_over = False
+fps = 100
+Score = 0
+lives = 5
+main_font = pygame.font.SysFont("comicsans", 50)
+
+#background
+background_image = pygame.transform.scale(pygame.image.load(os.path.join("images/background.png")), (width, height))
+
+#rocket
+rocket = pygame.transform.scale(pygame.image.load(os.path.join("images/vaccine.png")),(50,100))
+x = 400
+y = 600
+
+#enemy
+enemy_ship = pygame.transform.scale(pygame.image.load(os.path.join("images/virus.png")),(60,60))
+enemyx = random.randint(0,800)
+enemyy = 50
+
+#bullet
+f_ball = pygame.transform.scale(pygame.image.load(os.path.join("images/vaccine.png")),(25,50))
+bulletX = 0
+bulletY = y
+bulletX_change = 0
+bulletY_change = 15
+bullet_state = "ready"
+
+clock= pygame.time.Clock()
+
+def rock(x,y):
+	gameWindow.blit(rocket, (x,y))	# to display rocket on scree
+
+def enemy(enemyx,enemyy):
+	gameWindow.blit(enemy_ship, (enemyx,enemyy))
+
+def bullet_f(x, y):
+	global bullet_state
+	bullet_state = "fire"
+	gameWindow.blit(f_ball, (x + 15, y + 5))
+
+def shoot(enemyX, enemyY, bulletX, bulletY):
+    distance = math.sqrt(math.pow(enemyX - bulletX, 2) + (math.pow(enemyY - bulletY, 2)))
+    if distance < 40:
+        return True
+    else:
+        return False
+
+while not exit_game:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            exit_game = True
+
+    if event.type==pygame.KEYDOWN:
+	    if event.key == pygame.K_LEFT:
+	    	x = x-7
+	    	if x <= 0:
+	    		x=0
+	    	
+	    elif event.key == pygame.K_RIGHT:
+	    	x = x+7
+	    	if x >= 800:
+	    		x = 800
+
+	    elif event.key == pygame.K_SPACE:
+	    	if bullet_state == "ready":
+	    		bulletX = x
+	    		bullet_f(bulletX, bulletY)
+	
+
+    lives_label = main_font.render(f"Lives: {lives}", 1, (0,0,0))  # create lives font
+    Score_label = main_font.render(f"Score: {Score}", 1, (0,0,0)) # create Score font
+    
+    gameWindow.blit(background_image, [0,0])	# to display background image
+    gameWindow.blit(lives_label, (10,10))	# to display lives on scree
+    gameWindow.blit(Score_label, (width - Score_label.get_width() - 10, 10))	# to display Score on scree
+    
+    if bulletY <=0:
+    	bulletY =y
+    	bullet_state = "ready"
+
+    if bullet_state == "fire":
+        bullet_f(bulletX, bulletY)
+        bulletY -= bulletY_change
+
+    rock(x,y)
+    enemyy += 2
+    enemy(enemyx,enemyy)
+    if enemyy == 800:
+    	enemyx = random.randint(0,800)
+    	enemyy = 50
+    	lives -= 1
+    if lives == 0:
+    	break
+
+    collision = shoot(enemyx, enemyy, bulletX, bulletY)
+    if collision:
+    	Score +=1
+    	enemyx = random.randint(0,800)
+    	enemyy = 50
+    	bulletY = 480
+    	bullet_state = "ready"
+
+
+    pygame.display.flip()
+    clock.tick(fps)
+
+pygame.quit()
+quit()
